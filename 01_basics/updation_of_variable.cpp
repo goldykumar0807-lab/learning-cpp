17f1ea65a2a05bc87d4cd3687b3c5a2eb074bf4c
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-13 16:11:58 +0530

    Add variable updation example in C++

---
+++ 01_basics/updation_of_variable.cpp
@@ -0,0 +1,11 @@
+//updation of variable.
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    x=10;
+    cout<<"value of  x is : "<<x<<endl;
+    x=x+10;
+    cout<<"value of x is : "<<x<<endl;
+    return 0;
+}
