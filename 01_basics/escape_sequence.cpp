532e8f3280f6a0f4273c4745bf04edd0bfdc9cb3
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-13 16:11:10 +0530

    Create escape_sequence.cpp for output examples
    Add a C++ program demonstrating escape sequences.

---
+++ 01_basics/escape_sequence.cpp
@@ -0,0 +1,9 @@
+// escape sequence.
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"hello world"<<endl;
+    cout<<"my brother name is yash.\n";
+    cout<<"my friend name is shaaan.";
+    return 0;
+}
