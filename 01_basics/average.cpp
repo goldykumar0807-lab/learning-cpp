e711b48171a89d78aabc3da85b5f7aa20667e235
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-13 16:14:30 +0530

    Add average calculation program in C++

---
+++ 01_basics/average.cpp
@@ -0,0 +1,13 @@
+//average of marks.
+#include <iostream>
+using namespace std;
+int main() {
+    float x1=80;
+    float x2=98;
+    float x3=78;
+    float x4=99;
+    float x5=56;
+    float average = (x1+x2+x3+x4+x5)/5;
+    cout<<"average of marks is : "<<average<<endl;
+    return 0;
+}
