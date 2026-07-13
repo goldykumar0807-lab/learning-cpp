0bd6e60fc13e798a4f4890c5bcf10345df7a70fa
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-13 16:13:04 +0530

    Add example of increment and decrement operators
    Demonstrate pre and post increment/decrement operators in C++.

---
+++ 01_basics/pre_and_post_increment_operators.cpp
@@ -0,0 +1,12 @@
+//pre and post increment and decrement operator.
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    cout<<x++<<endl; //post increment
+    cout<<++x<<endl; //pre increment
+    cout<<x--<<endl; //post decrement
+    cout<<--x<<endl; //pre decrement
+    cout<<x<<endl;
+    return 0;
+}
