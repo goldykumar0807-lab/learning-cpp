a8ac13dfd7d8b3578aa0d144e176c0cd512394ee
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-13 16:15:19 +0530

    Add area_of_circle.cpp to calculate circle area

---
+++ 01_basics/area_of_circle.cpp
@@ -0,0 +1,9 @@
+//calculation area of circle.
+#include <iostream>
+using namespace std;
+int main() {
+    float radius = 4.5;
+    float area = 3.14*radius*radius;
+    cout<<"area of circle : "<<area<<endl;
+    return 0;
+}
