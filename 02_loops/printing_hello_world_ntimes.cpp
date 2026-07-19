53eda31ba631e26f8b3cab73b3fd877884984970
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-19 23:15:14 +0530

    Implement loop to print 'hello world' n times
    Add a loop to print 'hello world' n times based on user input.

---
+++ 02_loops/printing_hello_world_ntimes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int num;
+    cout<<"enter a number : ";
+    cin>>num;
+    for(int i=1; i<=num; i++){
+        cout<<"hello world"<<endl;
+    }
+    return 0;
+}
