d53749e3b4474afcd60aacd0a1c404bdfe366e98
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-22 21:54:29 +0530

    Add traffic signal simulator in C++

---
+++ 01_basics/traffic_signal_simulator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char signal;
+
+    cout << "Enter signal (R, Y, G): ";
+    cin >> signal;
+
+    switch (signal) {
+        case 'R':
+        case 'r':
+            cout << "Stop";
+            break;
+
+        case 'Y':
+        case 'y':
+            cout << "Get Ready";
+            break;
+
+        case 'G':
+        case 'g':
+            cout << "Go";
+            break;
+
+        default:
+            cout << "Invalid Signal";
+    }
+
+    return 0;
+}
