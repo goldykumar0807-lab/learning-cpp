dbeccf0759bc975c6db2f6b7fdc7e1b5e1b9273c
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-25 23:29:19 +0530

    Add odd number triangle pattern printing program
    Implement a program to print an odd number triangle pattern based on user input for rows.

---
+++ 03_pattern_printing/odd_number_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"enter no of rows : ";
+    cin>>r;
+    for(int i=1;i<=r;i++){
+        int k=1;
+        for(int j=1;j<=i;j++){
+            cout<<k<<" ";
+            k+=2;
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
