f8558f4ae7a00f7d561d5c93fe23c87938e32411
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-25 11:56:24 +0530

    Implement sum of even digits in a given number

---
+++ 02_loops/sum-of_even_digits-of_a_given_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    int sum=0;
+    while(n>0){
+        int digit=n%10;
+        if(digit%2==0) sum+=digit;
+        n/=10;
+    }
+    cout<<sum<<" is the sum of even digits of number.";
+    return 0;
+}
