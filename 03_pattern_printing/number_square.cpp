186784372b9bdea2c88e4f28b320722fe49d5bf8
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-25 15:01:42 +0530

    Add number square pattern printing program
    This program prints a square pattern of numbers based on user input for the number of rows.

---
+++ 03_pattern_printing/number_square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m ;
+    cout<<"enter no of rows : ";
+    cin>>m ;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+        }
+    }
+
+
