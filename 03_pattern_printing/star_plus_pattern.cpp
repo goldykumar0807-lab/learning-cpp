197199ca7cd1bd851c16af03d82e5c425c95a8a5
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-25 23:48:52 +0530

    Add star plus pattern printing program
    This program prints a star pattern based on the number of rows input by the user. It displays a star in the middle row and middle column.

---
+++ 03_pattern_printing/star_plus_pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+int r; //r always odd.
+cout<<"enter no of rows : ";
+cin>>r;
+for(int i=1;i<=r;i++){
+    for(int j=1;j<=r;j++){
+        if(j==(r+1)/2) cout<<"*";
+        else if (i==(r+1)/2) cout<<"*";
+        else cout<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
