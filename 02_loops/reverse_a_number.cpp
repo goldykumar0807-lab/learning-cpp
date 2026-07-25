4a508aca9a5c297484f9fc1d3dddd021b1617218
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-25 10:27:07 +0530

    reverse_a_number.cpp for negative and positive numbers
    Refactor reverse number logic to handle negative inputs.

--- 02_loops/reverse_a_number.cpp
+++ 02_loops/reverse_a_number.cpp
@@ -1,18 +1,27 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int num, reverse = 0;
-
-    cout << "Enter a number: ";
-    cin >> num;
-
-    while (num != 0) {
-        reverse = reverse * 10 + num % 10;
-        num /= 10;
+int main(){
+    int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    int rev=0;
+    if(n<0){
+        n=-n;
+    while(n>0){
+        int digit=n%10;
+        rev*=10;
+        rev+=digit;
+        n/=10;
     }
-
-    cout << "Reversed number: " << reverse;
-
+    cout<<-rev<<" is the reverse of number";
+    }else{
+        while(n>0){
+        int digit=n%10;
+        rev*=10;
+        rev+=digit;
+        n/=10;
+    }
+    cout<<rev<<" is the reverse of number";
+    }
     return 0;
 }
