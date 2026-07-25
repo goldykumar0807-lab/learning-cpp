368f632eeddf386f14fd2a4a74ea0e93df84bb57
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-26 01:26:53 +0530

    Add hollow square grid pattern printing program
    This program prints a hollow square grid pattern based on user-defined rows and columns.

---
+++ 03_pattern_printing/hollow_square_grid_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"enter no of row and column : ";
+    cin>>r>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=c;j++){
+            if(i==1 || i==r) cout<<"*";
+            else if(i>=2 && i<r){
+                if(j==1 || j==c) cout<<"*";
+                else cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
