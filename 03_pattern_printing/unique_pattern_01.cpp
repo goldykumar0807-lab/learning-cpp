1ffdd4912884ca88472898a75d3247ac09155f62
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-27 20:23:32 +0530

    Add unique pattern printing in C++
    This program prints a unique pattern based on the number of rows input by the user, displaying an upper and lower portion of stars and spaces.

---
+++ 03_pattern_printing/unique_pattern_01.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"enter no of rows : ";
+    cin>>r;
+    //printing upper portion.
+    for(int i=1;i<=r;i++){
+        for(int j=r;j>=1;j--){
+           if(i+j>=r+1) cout<<"*";
+           else cout<<" ";
+        }
+        for(int j=1;j<r;j++){
+            if(i+j>=r) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+    //printing lower portion.
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=r;j++){
+            if(i+j<=r+1) cout<<"*";
+            else cout<<" ";
+        }
+        for(int j=r-1;j>=1;j--){
+            if(i+j<=r+1) cout<<"*";
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
