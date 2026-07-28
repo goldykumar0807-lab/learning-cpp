627ba4aa56d6d0d61b0bc3bfcddf5cd00c5f39bb
goldykumar0807-lab <goldykumar0807@gmail.com>
2026-07-28 14:44:17 +0530

    Add program to swap two numbers using a third variable

---
+++ 01_basics/swapping_two_numbers_by_using-third_variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter two numbers : ";
+    cin>>a>>b;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<" "<<b;
+}
